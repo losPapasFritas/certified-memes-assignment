671162128c02fb5bc63a05d6f11c86389d1c2b36
losPapasFritas <143848605+losPapasFritas@users.noreply.github.com>
2024-02-07 14:50:55 -0800

    added info to certificate options

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
 let unknown = {title: `uh idk lol`, desc:`Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce id arcu quis enim sollicitudin tincidunt in a arcu. Morbi facilisis ultrices neque eu feugiat.`,
-                 issuer:`Peppino`, bkg:`#89E1C4`, textColor:`#E09A89`};
-let certification = {title:`Certification`, };
+                 issuer:`Peppino`,issuerTitle:`CEO of Lorem`, certClass:`lime`, badge:`potato`};
+let certification = {title:`Certification`, desc: `This certificate certifies that the recipient is certified in certifying others.`, issuer: `BT-7274`, issuerTitle:`Robot`, certClass:`walmart`, badge:`potato`};
+let goofy = {title:`being silly goofy`, desc: `This certifies that the recipient is indeed a silly goofy guy, and does everything for silly goofy reasons.\n:3`, issuer: `Grain`, issuerTitle:`The Janitor`, certClass:`walmart`, badge:``};
