96ce9d557aeaa4655bc7b318312062774880a13b
losPapasFritas <143848605+losPapasFritas@users.noreply.github.com>
2024-02-07 14:23:41 -0800

    added an object

---
+++ script.js
@@ -0,0 +1,3 @@
+let unknown = {title: `uh idk lol`, desc:`Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce id arcu quis enim sollicitudin tincidunt in a arcu. Morbi facilisis ultrices neque eu feugiat.`,
+                 issuer:`Peppino`, bkg:`#89E1C4`, textColor:`#E09A89`};
+let certification = {title:`Certification`, };
