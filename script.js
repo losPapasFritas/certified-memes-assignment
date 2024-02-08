cd85d671357060ce8582c4e94ee6805365fa603c
losPapasFritas <143848605+losPapasFritas@users.noreply.github.com>
2024-02-08 15:45:35 -0800

    Edited html, js. Added images, css
    Added more html to account for the images. Added js to fix issues with the console.log statement returning html, added css to start styling the page, added images for future use

--- script.js
+++ script.js
@@ -2,12 +2,19 @@
 //                  issuer:`Peppino`,issuerTitle:`CEO of Lorem`, certClass:`lime`, badge:`potato`};
 // let certification = {title:`Certification`, desc: `This certificate certifies that the recipient is certified in certifying others.`, issuer: `BT-7274`, issuerTitle:`Robot`, certClass:`walmart`, badge:`potato`};
 // let goofy = {title:`being silly goofy`, desc: `This certifies that the recipient is indeed a silly goofy guy, and does everything for silly goofy reasons.\n:3`, issuer: `Grain`, issuerTitle:`The Janitor`, certClass:`walmart`, badge:``};
-if ( document.querySelectorAll('.button') !== null) {
-    let buttons = document.querySelectorAll('.button');
-    buttons.forEach(button => button.addEventListener('click', processForm))
-}
-
-document.getElementById("userInput").addEventListener("submit", function (eventData) {
-    eventData.preventDefault();
-    console.log(eventData);
+document.addEventListener("DOMContentLoaded", function () {
+    if (document.querySelectorAll('.button') !== null) {
+        let buttons = document.querySelectorAll('.button');
+        buttons.forEach(button => button.addEventListener('click', processForm))
+    }
+    document.getElementById("userInput").addEventListener("submit", function (e) {
+        e.preventDefault();
+        console.log(e.target);
+        var formData = new FormData(e.target);
+        formData = Object.fromEntries(formData);
+        // let nameOutput = document.getElementById('nameOutput');
+        // let info = document.getElementById('info');
+        // nameOutput.innerText = formData.name;
+        // info.innerText = formData.type;
+    })
 })
