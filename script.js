9bdf3db74d1d842f0a6717075d7e763489c6b566
losPapasFritas <143848605+losPapasFritas@users.noreply.github.com>
2024-02-09 15:48:50 -0800

    fixed issues
    Added functionality, added styles, added html

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
-// let unknown = {title: `uh idk lol`, desc:`Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce id arcu quis enim sollicitudin tincidunt in a arcu. Morbi facilisis ultrices neque eu feugiat.`,
-//                  issuer:`Peppino`,issuerTitle:`CEO of Lorem`, certClass:`lime`, badge:`potato`};
-// let certification = {title:`Certification`, desc: `This certificate certifies that the recipient is certified in certifying others.`, issuer: `BT-7274`, issuerTitle:`Robot`, certClass:`walmart`, badge:`potato`};
-// let goofy = {title:`being silly goofy`, desc: `This certifies that the recipient is indeed a silly goofy guy, and does everything for silly goofy reasons.\n:3`, issuer: `Grain`, issuerTitle:`The Janitor`, certClass:`walmart`, badge:``};
+// let unknown = {certClass:`lime`, badge:`potato`};
+// let certification = {issuerTitle:`Robot`, certClass:`walmart`, badge:`potato`};
+// let goofy = {certClass:`walmart`, badge:``};
 document.addEventListener("DOMContentLoaded", function () {
     if (document.querySelectorAll('.button') !== null) {
         let buttons = document.querySelectorAll('.button');
@@ -9,12 +8,52 @@ document.addEventListener("DOMContentLoaded", function () {
     }
     document.getElementById("userInput").addEventListener("submit", function (e) {
         e.preventDefault();
-        console.log(e.target);
         var formData = new FormData(e.target);
         formData = Object.fromEntries(formData);
-        // let nameOutput = document.getElementById('nameOutput');
-        // let info = document.getElementById('info');
-        // nameOutput.innerText = formData.name;
-        // info.innerText = formData.type;
+        let nameOutput = document.getElementById('nameOutput');
+        let info = document.getElementById('certTitle');
+        nameOutput.innerText = formData.userName;
+        info.innerText = formData.certType;
+        console.log(formData)
+        changeCert(formData.certType);
+        
     })
+
+    if(document.getElementById(`print`) !== null){
+        let printElement = document.getElementById(`print`);
+        printElement.addEventListener(`click`, function(e){
+            window.print();
+            console.log(`invoke print`)
+        })
+    }
+
+    let theDate = new Date().toLocaleDateString(`en-us`, {weekday:`long`, year:`numeric`, month:`numeric`, day:`numeric`})
+    let dateBox = document.getElementById(`dateInput`);
+    dateBox.innerText = theDate;
+
+    function changeCert(value){
+        let desc = document.getElementById(`certDesc`);
+        let name = document.getElementById(`certIssuer`);
+        let job = document.getElementById(`certIssuer`);
+        switch(value){
+          case `um idk lol`:
+            desc.innerText = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce id arcu
+            quis enim sollicitudin tincidunt in a arcu. Morbi facilisis ultrices neque eu feugiat.`
+            name.innerText = `Peppino`;
+            job.innerText = `Ceo Of Lorem`;
+            break;
+          case `Certification`:
+            desc.innerText = `This certificate certifies that the recipient is certified in
+            certifying others.`
+            name.innerText = `BT-7274`;
+            job.innerText = `Robot`;
+            break;
+          case `being silly goofy`:
+            desc.innerText = `This certifies that the recipient is indeed a silly goofy guy, and does
+            everything for silly goofy reasons.\n:3`
+            name.innerText = `Grain`;
+            job.innerText = `Janitor`;
+            break;
+        }
+    };
 })
